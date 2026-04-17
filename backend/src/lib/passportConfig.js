6eabea38aadf229795c6a46c27de08a45f0b8c75
HoangphucPhan <phuc20513@gmail.com>
2026-04-17 11:41:55 +0700

    fix proxy

--- backend/src/lib/passportConfig.js
+++ backend/src/lib/passportConfig.js
@@ -4,7 +4,7 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BASE_URL}/api/auth/login/google/callback`
+  callbackURL: `https://api.hwagfu.dev/api/auth/login/google/callback`
 },
 (_, __, profile, done) => done(null, profile)
 ));
